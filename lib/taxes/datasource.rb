fc893c59c4690320e598403982fd59bd01a520c3
Mickael Pham <mickael.pham@gmail.com>
2018-05-06 19:20:53 -0700

    load files from data source, write output

--- lib/taxes/datasource.rb
+++ lib/taxes/datasource.rb
@@ -1,13 +1,20 @@
+# frozen_string_literal: true
+
+require 'json'
+
 module Taxes
   # Loads the different data sources (tax brackets and income) from files
   module Datasource
     class << self
       # Expects JSON
       def load_tax_brackets(filename)
+        file = File.read(filename)
+        JSON.parse(file, symbolize_names: true)
       end
 
       # One income per line
       def load_incomes(filename)
+        File.readlines(filename).map(&:chomp).map(&:to_i)
       end
     end
   end
